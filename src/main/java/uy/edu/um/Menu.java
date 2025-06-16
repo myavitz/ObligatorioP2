b01909f74579b07676ecd9d153e1251845689afe
MBentura <mbentura@correo.um.edu.uy>
2025-06-16 20:25:35 -0300

    SubMenu
    Hice el submenu y agregue unas lineas al gitignore que metia la carpeta con la metadata a la carpta del obligarorio

--- src/main/java/uy/edu/um/Menu.java
+++ src/main/java/uy/edu/um/Menu.java
@@ -13,7 +13,7 @@ public class Menu {
             System.out.println("Bienvenido al cine UMovie");
             System.out.println("Seleccione una de las siguientes opciones: ");
             System.out.println("1- Cargar los datos.");
-            System.out.println("2- No hago nada. ");
+            System.out.println("2- Ejecutar consultas.");
             System.out.println("3- Salir. ");
             opcion = sc.nextInt();
             sc.nextLine();
@@ -24,7 +24,7 @@ public class Menu {
                     loader.cargarDatos();
                 }
                 case 2 -> {
-                    System.out.println("No hago nada");
+                    mostrarSubMenu(sc);
                 }
                 case 3 ->{
                     System.out.println("Saliendo..");
@@ -34,5 +34,40 @@ public class Menu {
             }
 
         }while(opcion != 3);
+    }
+
+    private void mostrarSubMenu(Scanner sc){
+        int opcionSubMenu;
+
+        do {
+            System.out.println("Seleccione una de las siguientes opciones");
+            System.out.println("1- Top 5 de las peliculas que mas calificaciones por idioma");
+            System.out.println("2- Top 10 de las peliculas que mejor calificacion media tienen por parte de los usuarios");
+            System.out.println("3- Top 5 de las colecciones que mas ingresos generaron");
+            System.out.println("4- Top 10 de los directores que mejor calificacion tienen");
+            System.out.println("5- Actor con mas calificaciones recibidas en cada mes del año");
+            System.out.println("6- Usuarios con mas calificaciones por genero");
+            System.out.println("7- Volver al menu principal");
+            opcionSubMenu = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcionSubMenu) {
+                case 1:
+                    System.out.println("Consulta 1");
+                case 2:
+                    System.out.println("Consulta 2");
+                case 3:
+                    System.out.println("Consulta 3");
+                case 4:
+                    System.out.println("Consulta 4");
+                case 5:
+                    System.out.println("Consulta 5");
+                case 6:
+                    System.out.println("Consulta 6");
+                case 7:
+                    System.out.println("Volviendo al menu principal");
+                    opcionSubMenu = 7;
+            }
+        }while (opcionSubMenu !=7);
     }
 }
